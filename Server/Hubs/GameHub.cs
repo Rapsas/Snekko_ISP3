0f17ab36a988349d846fde78bc02f979a4e57982
Paulius Ložys <pauliuslozys@gmail.com>
2021-09-13 13:48:55 +0300

    Refactoring and added server project

---
+++ Server/Hubs/GameHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task SendPositions(string player, string playstate)
+        {
+            await Clients.All.SendAsync("RecievePositions", player, playstate);
+        }
+    }
+}
