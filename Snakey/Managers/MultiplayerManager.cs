6788b0a6d00071615edae1b1a746b799f5851bee
Paulius Ložys <pauliuslozys@gmail.com>
2021-09-28 16:17:59 +0300

    Server: add disconnect function

--- Snakey/Managers/MultiplayerManager.cs
+++ Snakey/Managers/MultiplayerManager.cs
@@ -8,20 +8,21 @@ namespace Snakey.Managers
     public class MultiplayerManager
     {
         public HubConnection Connection { get; set; }
-        public string ID { get { return Connection.ConnectionId; }  }
-
         public MultiplayerManager(string url)
         {
             Connection = new HubConnectionBuilder()
                .WithUrl(new Uri(url))
                .WithAutomaticReconnect()
                .Build();
-
         }
 
         public async Task ConnectToServer()
         {
             await Connection.StartAsync();
+        }
+        public async Task DisconnectFromServer()
+        {
+            await Connection.StopAsync();
         }
     }
 }
