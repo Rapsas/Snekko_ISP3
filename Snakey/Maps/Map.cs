54ad6e8326dd8a40e7527082782141c63b5a4fb4
Paulius Ložys <pauliuslozys@gmail.com>
2021-09-30 10:05:46 +0300

    Added player collision

--- Snakey/Maps/Map.cs
+++ Snakey/Maps/Map.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Common.Utility;
+using System.Collections.Generic;
 using System.Windows.Shapes;
 
 namespace Snakey.Maps
@@ -6,5 +7,7 @@ namespace Snakey.Maps
     public abstract class Map
     {
         public List<Line> GridLines { get; set; } = new();
+        public List<Vector2D> Obsticles { get; set; } // Only initialize when needed
+        public abstract void PlayerCollisionCheck();
     }
 }
