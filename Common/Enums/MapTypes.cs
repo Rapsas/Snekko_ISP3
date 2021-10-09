4652f06d4174afae032787c858e40b291f3259c2
lukasdevenas <lukas.devenas@ktu.edu>
2021-10-09 15:13:24 +0300

    Added Factory Method Logic

---
+++ Common/Enums/MapTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Enums
+{
+    public enum MapTypes
+    {
+        Basic,
+        Advance,
+        Expert
+    }
+}
---
+++ Snakey/Factories/MapFactory.cs
@@ -0,0 +1,24 @@
+﻿using Common.Enums;
+using Snakey.Maps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snakey.Factories
+{
+    class MapFactory : IMapFactory
+    {
+        public Map CreateMap(MapTypes map)
+        {
+            return map switch
+            {
+                MapTypes.Basic => new BasicMap(),
+                MapTypes.Advance => new AdvanceMap(),
+                MapTypes.Expert => new ExpertMap(),
+                _ => null,
+            };
+        }
+    }
+}
