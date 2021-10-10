2f01a115203acd36a31a3ac65c65f39430727c8a
Paulius Ložys <pauliuslozys@gmail.com>
2021-10-10 18:21:00 +0300

    Added advanced and expert map collision

--- Snakey/Maps/AdvanceMap.cs
+++ Snakey/Maps/AdvanceMap.cs
@@ -1,12 +1,42 @@
-﻿using Snakey.Models;
+﻿using Snakey.Managers;
+using Snakey.Models;
 using System;
 
 namespace Snakey.Maps
 {
     class AdvanceMap : Map
     {
+        GameState _gameState = GameState.Instance;
         public override void MapCollisionCheck()
         {
+            var player = _gameState.Player;
+
+            if (_gameState.Player.HeadLocation.X < 0)
+            {
+                player.HeadLocation = new((int)_gameState.GameArea.ActualWidth, player.HeadLocation.Y);
+            }
+            else if (player.HeadLocation.Y < 0)
+            {
+                player.HeadLocation = new(player.HeadLocation.X, (int)_gameState.GameArea.ActualHeight);
+            }
+            else if (player.HeadLocation.X >= _gameState.GameArea.ActualWidth
+                || player.HeadLocation.Y >= _gameState.GameArea.ActualHeight)
+            {
+
+                // Wrap around map
+                player.HeadLocation = new(
+                    player.HeadLocation.X % (int)_gameState.GameArea.ActualWidth,
+                    player.HeadLocation.Y % (int)_gameState.GameArea.ActualHeight);
+            }
+
+            foreach (var (location, _) in Obsticles)
+            {
+                if (location.IsOverlaping(player.HeadLocation))
+                {
+                    player.IsDead = true;
+                    return;
+                }
+            }
         }
     }
 }
--- Snakey/Maps/ExpertMap.cs
+++ Snakey/Maps/ExpertMap.cs
@@ -1,12 +1,24 @@
-﻿using Snakey.Models;
+﻿using Snakey.Managers;
+using Snakey.Models;
 using System;
 
 namespace Snakey.Maps
 {
     class ExpertMap : Map
     {
+        GameState _gameState = GameState.Instance;
+
         public override void MapCollisionCheck()
         {
+            var player = _gameState.Player;
+            foreach (var (location, _) in Obsticles)
+            {
+                if (location.IsOverlaping(player.HeadLocation))
+                {
+                    player.IsDead = true;
+                    return;
+                }
+            }
         }
     }
 }
