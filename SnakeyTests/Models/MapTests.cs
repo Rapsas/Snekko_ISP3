bcbd1743d710dc10adb83c905595a726d72ebe01
Mykolas Paulauskas <33966587+Rapsas@users.noreply.github.com>
2021-11-07 19:30:19 +0200

    update

--- SnakeyTests/Models/MapTests.cs
+++ SnakeyTests/Models/MapTests.cs
@@ -11,29 +11,35 @@ using Common.Utility;
 using System.Windows.Shapes;
 using Snakey.Maps;
 using Snakey.Bridge;
+using Snakey.Managers;
 
 namespace Snakey.Models.Tests
 {
     public class MapTests
     {
-        [Theory]
-        [InlineData()]
+        [StaFact]
         public void MapCollisionCheckTest()
         {
-            var thread = new Thread(() => {
-                var gameState = Mocks.GetGameState();
+            GameState gameState = null;
+            while (gameState == null)
+            {
+                gameState = Mocks.GetGameState();
+            }
+            gameState.Player.HeadLocation = new(0, 0);
+            List<(Vector2D, Rectangle)> obsticles = new();
+            obsticles.Add((new Vector2D(0, 0), new Rectangle()));
+            Map testingObject = new ExpertMap(new ExpertCollision());
+            testingObject.Obsticles = obsticles;
 
-                gameState.Player.HeadLocation = new(0, 0);
-                List<(Vector2D, Rectangle)> obsticles = new();
-                obsticles.Add((new Vector2D(0, 0), new Rectangle()));
-                Map testingObject = new ExpertMap(new ExpertCollision());
-                testingObject.Obsticles = obsticles;
-
-                testingObject.MapCollisionCheck();
+            testingObject.MapCollisionCheck();
+            try
+            {
                 Assert.True(gameState.Player.IsDead);
-            });
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
+            }
+            finally
+            {
+                Mocks.ReleaseGameState();
+            }
         }
     }
 }
