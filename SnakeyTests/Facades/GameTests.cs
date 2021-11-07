de65655d5ae27487918219769f2d6e437effae22
RainbowDog98 <42966213+RainbowDog98@users.noreply.github.com>
2021-11-07 19:17:49 +0200

    Decorator thread safe

--- SnakeyTests/Facades/GameTests.cs
+++ SnakeyTests/Facades/GameTests.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using Common.Enums;
 using Snakey.Maps;
 using System.Windows.Input;
+using Snakey.Managers;
+using SnakeyTests.Mocks;
 
 namespace Snakey.Facades.Tests
 {
@@ -16,58 +18,97 @@ namespace Snakey.Facades.Tests
         [StaFact]
         public void InitTest()
         {
+            GameState gameState = null;
+            while (gameState == null)
+            {
+                gameState = Mocks.GetGameState();
+            }
+
             Game game = new();
             MainWindow window = new();
             game.Init(window);
-            Assert.True(game.Publisher != null);
-            Assert.True(game.Server != null);
-            Assert.True(game.Window != null);
-            Assert.True(game.GameState != null);
+            try
+            {
+                Assert.True(game.Publisher != null);
+                Assert.True(game.Server != null);
+                Assert.True(game.Window != null);
+                Assert.True(game.GameState != null);
+            }
+            finally
+            {
+                Mocks.ReleaseGameState();
+            }
 
         }
 
         [StaFact]
         public void SwitchToLevelTest()
         {
+            GameState gameState = null;
+            while (gameState == null)
+            {
+                gameState = Mocks.GetGameState();
+            }
+
             Game game = new Game();
             MainWindow window = new();
             game.Init(window);
 
-            game.SwitchToLevel(MapTypes.Expert);
-            Assert.IsType<ExpertMap>(game.GameState.GameMap);
-            game.SwitchToLevel(MapTypes.Expert);
+            try
+            {
+                game.SwitchToLevel(MapTypes.Expert);
+                Assert.IsType<ExpertMap>(game.GameState.GameMap);
+                game.SwitchToLevel(MapTypes.Expert);
 
-            game.SwitchToLevel(MapTypes.Advance);
-            Assert.IsType<AdvanceMap>(game.GameState.GameMap);
-            game.SwitchToLevel(MapTypes.Expert);
+                game.SwitchToLevel(MapTypes.Advance);
+                Assert.IsType<AdvanceMap>(game.GameState.GameMap);
+                game.SwitchToLevel(MapTypes.Expert);
 
-            game.SwitchToLevel(MapTypes.Basic);
-            Assert.IsType<BasicMap>(game.GameState.GameMap);
-            game.SwitchToLevel(MapTypes.Expert);
+                game.SwitchToLevel(MapTypes.Basic);
+                Assert.IsType<BasicMap>(game.GameState.GameMap);
+                game.SwitchToLevel(MapTypes.Expert);
+            }
+            finally
+            {
+                Mocks.ReleaseGameState();
+            }
         }
 
         [StaFact]
         public void HandleKeyboardTest()
         {
+            GameState gameState = null;
+            while (gameState == null)
+            {
+                gameState = Mocks.GetGameState();
+            }
+
             Game game = new Game();
             MainWindow window = new();
             game.Init(window);
 
-            game.HandleKeyboard(Key.S);
-            Assert.True(game.GameState.Player.CurrentMovementDirection == MovementDirection.Down);
-            game.GameState.Player.Move();
+            try
+            {
+                game.HandleKeyboard(Key.S);
+                Assert.True(game.GameState.Player.CurrentMovementDirection == MovementDirection.Down);
+                game.GameState.Player.Move();
 
-            game.HandleKeyboard(Key.A);
-            Assert.True(game.GameState.Player.CurrentMovementDirection == MovementDirection.Left);
-            game.GameState.Player.Move();
+                game.HandleKeyboard(Key.A);
+                Assert.True(game.GameState.Player.CurrentMovementDirection == MovementDirection.Left);
+                game.GameState.Player.Move();
 
 
-            game.HandleKeyboard(Key.W);
-            Assert.True(game.GameState.Player.CurrentMovementDirection == MovementDirection.Up);
-            game.GameState.Player.Move();
+                game.HandleKeyboard(Key.W);
+                Assert.True(game.GameState.Player.CurrentMovementDirection == MovementDirection.Up);
+                game.GameState.Player.Move();
 
-            game.HandleKeyboard(Key.D);
-            Assert.True(game.GameState.Player.CurrentMovementDirection == MovementDirection.Right);
+                game.HandleKeyboard(Key.D);
+                Assert.True(game.GameState.Player.CurrentMovementDirection == MovementDirection.Right);
+            }
+            finally
+            {
+                Mocks.ReleaseGameState();
+            }
         }
     }
 }
