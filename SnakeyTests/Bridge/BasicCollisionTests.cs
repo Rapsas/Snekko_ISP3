31c5a44f3d680a3f6e7f75118b3a57d8d6cb421a
Mykolas Paulauskas <33966587+Rapsas@users.noreply.github.com>
2021-11-07 18:35:47 +0200

    Bridge pt 3

--- SnakeyTests/Bridge/BasicCollisionTests.cs
+++ SnakeyTests/Bridge/BasicCollisionTests.cs
@@ -14,15 +14,42 @@ namespace Snakey.Bridge.Tests
 {
     public class BasicCollisionTests
     {
-        [StaTheory]
-        [InlineData(0, 0, 0, 0)]
-        public void MapCollisionTest(int playerX, int playerY, int objX, int objY)
+        [StaFact]
+        public void MapCollisionTestUpper()
         {
             var gameState = Mocks.GetGameState();
 
-            gameState.Player.HeadLocation = new(playerX, playerY);
+            gameState.Player.HeadLocation = new(-40, 0);
             List<(Vector2D, Rectangle)> obsticles = new();
-            obsticles.Add((new Vector2D(objX, objY), new Rectangle()));
+            obsticles.Add((new Vector2D(0, 0), new Rectangle()));
+
+            BasicCollision collision = new();
+            collision.MapCollision(obsticles);
+            Assert.False(gameState.Player.IsDead);
+        }
+
+        [StaFact]
+        public void MapCollisionTestLeft()
+        {
+            var gameState = Mocks.GetGameState();
+
+            gameState.Player.HeadLocation = new(0, -40);
+            List<(Vector2D, Rectangle)> obsticles = new();
+            obsticles.Add((new Vector2D(0, 0), new Rectangle()));
+
+            BasicCollision collision = new();
+            collision.MapCollision(obsticles);
+            Assert.False(gameState.Player.IsDead);
+        }
+
+        [StaFact]
+        public void MapCollisionTestBottomAndRight()
+        {
+            var gameState = Mocks.GetGameState();
+
+            gameState.Player.HeadLocation = new(int.MaxValue, 0);
+            List<(Vector2D, Rectangle)> obsticles = new();
+            obsticles.Add((new Vector2D(0, 0), new Rectangle()));
 
             BasicCollision collision = new();
             collision.MapCollision(obsticles);
